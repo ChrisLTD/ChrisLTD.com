00ae609872b64e0dca979ead3fc386bd512abf08
Chris Johnson <chrisltd@gmail.com>
2013-04-02 13:14:30 -0400

    Fixed bug in old IEs

--- js/script.js
+++ js/script.js
@@ -3,21 +3,29 @@
 // Created October 2012
 
 $(function(){
-  
+
+// Make sure this isn't ie8 or older
+if(!$('html').hasClass('lt-ie9')){
+	
+	// Add menu toggle to the top of the page
+	$('header > .container').prepend('<a class="menu_toggle" data-toggle=".main_nav" title="Show/Hide Menu"><i class="ss-icon ss-standard ss-rows"></i><b>Show/Hide Menu</b></a>');  
+	
+	// Initialize source code highlighting
+	hljs.tabReplace = '  ';
+	hljs.initHighlightingOnLoad();
+	
+	// Duplicate menu at the bottom of the page
+	$('#footer > .container').prepend('<nav class="footer_nav"></nav>');
+	$('.main_nav > ul').clone().appendTo('.footer_nav');
+	
+}
+
 // Toggle
 $('[data-toggle]').click(function(event) {
   event.preventDefault();
   toggleTarget = $(this).data('toggle');
   $(toggleTarget).slideToggle('fast');
 });
-
-// Duplicate menu at the bottom of the page
-$('#footer > .container').prepend('<nav class="footer_nav"></nav>');
-$('.main_nav > ul').clone().appendTo('.footer_nav');
-
-// Initialize source code highlighting
-hljs.tabReplace = '  ';
-hljs.initHighlightingOnLoad();
 
 });
 
