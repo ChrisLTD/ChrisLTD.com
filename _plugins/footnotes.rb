11e8c622eb5f77f7d5fd05e6c4ffeafd5481c2ae
Chris Johnson <chrisltd@gmail.com>
2013-04-02 11:07:57 -0400

    Updated docs and made random use a bigger number

--- _plugins/footnotes.rb
+++ _plugins/footnotes.rb
@@ -1,10 +1,10 @@
 # Title: Footnote Liquid Filter
-# Author: Syeong Gan
-# Description: Liquid filters to modify footnote markup (useful for blog index
+# Author: Chris Johnson
+# Description: Liquid filters to modify footnote markup. Useful for blog index
 #     pages where the same footnote reference may be used multiple times as
-#     articles are compiled)
+#     articles are compiled. Also for rss feeds where footnote links won't work.
 #
-# http://syeong.jcsg.com/2012/07/07/octopress-footnote-problem/
+# Based on code from: Syeong Gan http://syeong.jcsg.com/2012/07/07/octopress-footnote-problem/
 #
 # Example Usage:
 #   <div class="entry-content">{{ content | excerpt | remove_footnote_link }}</div>
@@ -12,13 +12,11 @@
 
 module FootnoteLiquidFilters
 	# Appends a random integer to the footnote reference link
-	#     Note, this is a hackish workaround (not foolproof if the same random
-	#     happens to be generated twice)
 	def rename_footnote_link(input)
-		random = rand(999)
+		random = rand(9999)
 		input.gsub(/fn:\d+/, '\0-'+"#{random}").gsub(/fnref:\d+/, '\0-'+"#{random}")
 	end
-	# Removes footnote links entirely
+	# Removes footnote hrefs entirely
 	def remove_footnote_link(input)
 		input.gsub(/ href='#(fn|fnref):\d+'/, '').gsub(/&#8617;/, '')
 	end
