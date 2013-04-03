49078b8446b5eb5170ba399a7a54c483a4344be9
Chris Johnson <chrisltd@gmail.com>
2013-04-03 09:26:58 -0400

    Turned on caching for sass

--- compileassets.sh
+++ compileassets.sh
@@ -5,4 +5,4 @@ packr js/jquery.yosimplefilter.js > js/jquery.yosimplefilter-ck.js
 packr js/portfolio.js > js/portfolio-ck.js
 packr js/script.js > js/script-ck.js
 
-sass scss/styles.scss css/styles.css --style compressed --no-cache
+sass scss/styles.scss css/styles.css --style compressed
