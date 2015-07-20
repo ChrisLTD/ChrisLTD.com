9f2a88d45164b4b82d684394550ee360a0fbd9a5
Chris Johnson <chrisltd@gmail.com>
2015-07-20 16:57:08 -0400

    Switched *-ck.js to *.min.js

--- compileassets.sh
+++ compileassets.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-packr js/blog.js > js/blog-ck.js
-packr js/jquery.yosimplefilter.js > js/jquery.yosimplefilter-ck.js
-packr js/portfolio.js > js/portfolio-ck.js
-packr js/script.js > js/script-ck.js
+packr js/blog.js > js/blog.min.js
+packr js/jquery.yosimplefilter.js > js/jquery.yosimplefilter.min.js
+packr js/portfolio.js > js/portfolio.min.js
+packr js/script.js > js/script.min.js
 
 sass scss/styles.scss css/styles.css --style compressed
