113e0710792f09f0c5dad4b36229edda58369812
Chris Johnson <chrisltd@gmail.com>
2020-09-22 16:31:32 -0500

    removed console safety script

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
-// Script by Chris Johnson 
+// Script by Chris Johnson
 // http://chrisltd.com
 // Created October 2012
 
 $(function(){
 
-// Make sure this isn't ie8 or older
-if(!$('html').hasClass('lt-ie9')){
+  // Make sure this isn't ie8 or older
+  if(!$('html').hasClass('lt-ie9')){
 
-	// Duplicate menu at the bottom of the page
-	$('#footer > .container').prepend('<nav class="footer_nav"></nav>');
-	$('.main_nav > ul').clone().appendTo('.footer_nav');
+    // Duplicate menu at the bottom of the page
+    $('#footer > .container').prepend('<nav class="footer_nav"></nav>');
+    $('.main_nav > ul').clone().appendTo('.footer_nav');
 
-}
+  }
 
-// Toggle
-$('[data-toggle]').click(function(event) {
-  event.preventDefault();
-  toggleTarget = $(this).data('toggle');
-  $(toggleTarget).slideToggle('fast');
+  // Toggle
+  $('[data-toggle]').click(function(event) {
+    event.preventDefault();
+    toggleTarget = $(this).data('toggle');
+    $(toggleTarget).slideToggle('fast');
+  });
+
 });
-
-});
-
-// make it safe to use console.log always
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
-{console.log();return window.console;}catch(err){return window.console={};}})());
-
