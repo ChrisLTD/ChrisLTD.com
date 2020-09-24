ec05f3ffb1975772934bfe8d1db6815740f29ddb
Chris Johnson <chrisltd@gmail.com>
2020-09-24 17:43:45 -0500

    added escape clauses for js files

--- js/blog.js
+++ js/blog.js
@@ -1,13 +1,18 @@
 $(function(){
+	if (!$('body').hasClass('blog')) {
+		return;
+	}
+
   // Fluid videos
 	$('article').fitVids();
-	
+
 	// Reformat footnotes
-	$('.footnotes li > [rev="footnote"]').each(function(index) {
-		appendTarget = $(this).parents('li').children('p:last-of-type');
-		$(this).appendTo(appendTarget);
-	}).fadeIn(200);
-	
+	// $('.footnotes li > [rev="footnote"]').each(function(index) {
+	// 	console.log('reformating footnote');
+	// 	appendTarget = $(this).parents('li').children('p:last-of-type');
+	// 	$(this).appendTo(appendTarget);
+	// }).fadeIn(200);
+
 	$.bigfoot();
 
 });
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,10 +1,14 @@
 $(function(){
-  
+
+	if (!$('body').hasClass('portfolio')) {
+    return;
+  }
+
   var settings = {
-		'childObject'         : '.portfolio_item',           // Targets to be filtered
-		'filterObject' : '.filter'              // Next and Previous button tag
+		'childObject': '.portfolio_item',      // Targets to be filtered
+		'filterObject': '.filter'              // Next and Previous button tag
 	};
-  
+
   $('.portfolio [role="main"] .container').YoSimpleFilter(settings);
-  
+
 });
