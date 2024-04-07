a0a74bdac2996c2f5198c85352ad073494963118
Chris Johnson <chrisltd@gmail.com>
2024-04-07 13:17:16 -0400

    adding bundle exec to build commands

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-packr js/blog.js > js/blog.min.js
-packr js/jquery.yosimplefilter.js > js/jquery.yosimplefilter.min.js
-packr js/bigfoot.js > js/bigfoot.min.js
-packr js/portfolio.js > js/portfolio.min.js
-packr js/script.js > js/script.min.js
-sass scss/styles.scss css/styles.css --style compressed
+bundle exec packr js/blog.js > js/blog.min.js
+bundle exec packr js/jquery.yosimplefilter.js > js/jquery.yosimplefilter.min.js
+bundle exec packr js/bigfoot.js > js/bigfoot.min.js
+bundle exec packr js/portfolio.js > js/portfolio.min.js
+bundle exec packr js/script.js > js/script.min.js
+bundle exec sass scss/styles.scss css/styles.css --style compressed
 
 bundle exec jekyll build
